ac8238048a375ae09654f37ae775ff5e6ac97744
basha <skchand175@gmai.com>
2024-03-22 02:46:23 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,479 @@
+
+
+// let num = 25;
+// console.log(num);
+// var name="my name is chand";
+// let name="my name is chand";
+// const name ="my name is chand";
+//
+//console.log(sum);
+// const num1=50;
+// {
+//     const num2=100;
+//     {
+// const num3=150;
+// {
+//     console.log(num1+num2+num3)
+// }
+//     }
+// }
+// var a='hello';
+// console.log('a');
+
+// let num=125;
+// console.log(num);
+// let isfailed =true;
+// console.log(isfailed);
+// var num1=null;
+// console.log(num1);
+// let number=(null);
+// console.log(number);
+// const name ="sajeed and sajid";
+// console.log( name);
+
+
+// let num2=128+150-55;
+// console.log(num2);
+// let sum=20;
+// sum=15+20;
+// sum=200-20*0+40;
+// console.log(sum);
+
+
+// let A=2;  
+//  let B=4;
+
+//  A=A+B
+//  B=A-B
+// A=A-B
+
+// console.log(A)
+// console.log(B)
+
+
+
+// // let age=20;
+// // if(age>25){
+// //     console.log(" ur eligible to love ");
+    
+// // }
+// // else if (age =20){
+// //     console.log("ur in a queue");
+// // }
+
+// // else{
+// //   console.log("ur not elgible to love");
+// // }
+
+// let amt=4000;
+// switch(amt) { 
+//     case 2000>=5000 :
+//         console.log("enter multipul of 500");
+//         break;
+// case 2800 :
+// console.log("elgible to take amount");
+// break;
+// case 120000:
+//     console.log("not elgible to take amount");
+// break;
+// default:
+//     console.log("unabe to dispatch amount");
+// break;
+
+// }
+
+// // let percentage=100;
+// // switch(percentage){
+// // case 40:
+// //     console.log( "Buy a pen.")
+// // }
+
+// // let percentage=82;
+// // if(percentage<=50){
+// //     console.log("buy a pen");
+// // }
+ 
+// // else if( percentage<=60){
+// //     console.group("buy a book")
+// // }
+// // else if( percentage<=70){
+// //     console.group("buy a bag")
+// // }
+// // else if( percentage<=80){
+// //     console.group("buy a watch")
+// // }
+// // else if( percentage<=90){
+// //     console.group("buy a mobile")
+// // }
+// // else if( percentage<=100){
+// //     console.group("buy a laptop")
+// // }
+// // else{
+// //     console.log("fail" )
+// // }
+
+
+
+
+
+
+// // let  N=2;
+// // if(N%2==0){
+// //     console.log("the number is even");
+// //     console.log(N);
+// //     console.log(N*2);
+// // }
+// // else{
+// //     console.log("then the number is odd");
+// //     console.log(N);
+// //     console.log(N+2);
+// // }
+
+
+// let num1 = parseFloat(prompt("Enter the first number:"));
+//  let operation =prompt("enter the operation(+-*/):");
+//  let num2 = parseFloat(prompt("enter the second number:")); 
+//  let result;
+//  if(operation =="+"){
+//     result=num1+num2;
+//  }
+//  else if (operation=="-"){
+//    result=num1-num2;
+
+// }
+//  else if (operation=="*"){
+//     result=num1*num2;
+    
+//  }
+// else if (operation=="/"){
+//     if(num2==0) {
+//         result ="cannot divide by zero";
+//     } else {
+//         result=num1/num2;
+//    }
+// }
+//      else {
+//        result= "invalid operation";
+//   }
+
+//  console.log("result:",result);
+
+
+
+
+
+// for (i=0;i<=4;i++)
+// { 
+//     console.log("sajeed and chand");
+// }
+
+// a=0
+// while(a<=10)
+// {
+//     console.log("sajeed and chand");
+//     a++;
+    
+// }
+
+// a=20
+// while(a>0) 
+// {
+//     console.log(a)
+//     a--;
+// }               
+
+ 
+
+
+
+// a=1;
+// do{
+//     console.log(a);
+//     a++
+// }
+// while(a<=9);
+
+
+
+
+// let sum=0;
+//  for(a=1; a<=10; a++)
+// {
+//  sum+=a; 
+ 
+// }
+
+// console.log("1+2+3+......+10 = " , sum);
+
+
+
+
+
+
+
+
+//  let sum = 0 
+//    a=0;
+// while(a<=10){
+//        sum+=a;
+    
+//    a++;
+  
+// }
+
+// console.log("1+2+3+4+5+6+7+8+9+10=", sum); 
+
+// console.log(12+"+"+12+"="+24)
+
+// let giveNumbers=prompt("enter")
+// let result=0
+// let res=(""+giveNumbers)
+
+// for(let i=giveNumbers-1;i>=1;i--){
+    
+//     if(result==0){
+//     let data=giveNumbers*i
+//     result=data
+//     console.group(giveNumbers +"*" +i+"="+result)
+//     }
+//     else{
+//         let data=result*i
+//         console.group(result +"*" +i+"="+data)
+//     result=data
+    
+//     }
+    
+// }
+
+
+
+// let giveNumbers=prompt("enter")
+// let result=0
+// let res=(""+giveNumbers)
+// for(let i=giveNumbers-1;i>=1;i--){
+//     let data=giveNumbers*i
+//     console.log(giveNumbers+"*"+i+"="+data)
+//     res+=("*"+i)
+//     giveNumbers=data
+// }
+
+// console.group(res+"="+giveNumbers)
+
+
+
+// function printMultiplicationTable(number, range)
+//  {
+//     console.log(`Multiplication table for ${number} up to ${range}:`);
+//     for (let i = 1; i <= range; i++) {
+//         console.log(`${number} x ${i} = ${number * i}`);
+//     }
+// }
+// const number = 20;
+// const range = 22; 
+// printMultiplicationTable(number, range);
+
+
+
+
+// function printMultiplicationTable(num , range){
+//     console.log(`multiplication table for ${num} up to ${ range}:`);
+// for( let a= 1; a <= range; a++){
+// console.log(`${num} x ${ a} =${ num * a}`);
+
+// }
+// }
+// let num=15; 
+// let range= 20;
+// printMultiplicationTable( num ,range);
+
+
+// int a = 0;
+// for(a; a<5; a++);
+// console.log(a);
+
+
+
+// var arr=[1,2,3];
+// var rev=arr.reverse();
+//  document.writeln(rev);
+
+// let a=[1,2,3,4,5]
+
+// let b=[4,5,6];
+
+// let c=[];
+// for(let i=0 ;i<a.length; i++){
+//     // console.log(i)
+//     console.log("index:"+i+"="+a[i])
+//     let element=a[i]
+
+// //     for(let j=0;j<b.length;j++){
+// //         if(element==b[j]){
+// //             c.push(element)
+//        }
+//     }
+    
+// }
+
+// console.log(c)
+
+
+
+//  const number = prompt("enter a number"); 
+// let isprime=true
+
+// // let  number= 3
+
+// for(let i=2;i<number;i++){
+// if(number%i==0){
+//     isprime=false
+//     break;
+// }
+// }
+
+// if(isprime && number>1){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+
+// let a =[2,3,5,6,9,8,4,1];
+// let b =[] // reversedArray
+// for(let i=a.length-1; i>=0 ; i--){
+//    b.push(a[i]);
+
+// }
+
+// console.log(a)
+
+// let a =[2,3,5,6,9,8,4,1];
+// let b = a[0];
+
+// for( let i =1 ; i <= a.length ; i++){
+//     if(a[i]> b){
+//         b = a[i];
+        
+
+//     }
+// }
+
+// console.log(b);
+
+
+
+
+// let  updatedId=2
+// let  newAge=38
+// const users = [
+//     { id: 1, name: "A", age: 25 },
+//     { id: 2, name: "B", age: 30 },
+//     { id: 3, name: "C", age: 22 }
+//   ];
+
+// for(let i=0; i<=users.length ; i++){
+//     if(users[i] && users[i].id && users[i].id==updatedId){
+//         users[i].age=newAge;
+//     }
+// }
+
+// console.log(users)
+
+ 
+// const n= 25;
+//   function series (n){
+//    let result=[];
+//     let a= 0;
+//    let b= 1;
+//    for(i = 0 ; i<n ; i++){
+//      result.push(a);
+//        let c = a+b;
+//       a=b;
+//          b=c;
+
+//        }
+//       console.log(result);
+//   }
+//  const result=series(n);
+//   console.log(result)
+
+
+// const areaOfaTriangl=(b,c)=>{
+//   let a = 0.5;
+//   let area = a*b*c;
+// return area;
+// }
+
+// let v1=prompt("enter first value")
+// let v2 =prompt("enter second value")
+// let result =areaOfaTriangl(v1,v2)
+// console.log(`Aera of a triangle:${result}`)
+
+
+
+// const areaOfaCircle=(r)=>{
+//   let a = 2*3.14 *r*r;
+   
+// return a;
+// }
+
+// let v1=prompt("enter the radius of a circle")
+// let result =areaOfaCircle(v1)
+// console.log(`Aera of a circle:${result}`)
+ 
+// let priceWithProfit=1000
+
+// let Profit=priceWithProfit/100*25
+
+// let realPrice=priceWithProfit-Profit
+// console.log(realPrice)
+
+// let loss=realPrice/100*25
+
+// let priceWithloss=realPrice-loss
+
+// console.log(priceWithloss)
+
+
+
+// let object ={
+//     houseRent : 20,
+//     food :20,
+//     TransformStream : 5,
+//     education :10, 
+//     otherExpenses: 20
+//  }
+// // let saveingAmount = 5000
+// for(let items of object){
+// //     console.log(items.object)
+// }
+
+
+
+function printNumbers() {
+    for (let i = 0; i <= 100; i++) {
+      console.log(i);
+    }
+  }
+  
+  // Call the function to print numbers from 0 to 100
+  printNumbers();
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
